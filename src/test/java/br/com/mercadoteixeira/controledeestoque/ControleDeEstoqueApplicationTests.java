c80ce3d6d2ef5181061cbf1e6738052f821b8ddf
conradospereira <conrado.spereira@gmail.com>
2022-06-14 13:40:32 -0300

    Project Mercado Teixeira.

---
+++ src/test/java/br/com/mercadoteixeira/controledeestoque/ControleDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadoteixeira.controledeestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeEstoqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
